0bb50f9ffad5a0a89edbcb9391abcbd46bd18baf
Ketan Jobanputra <ketan@nichethyself.com>
2021-08-07 15:04:45 +0530

    Modified my tests now I have two tests

--- src/test/java/webDriverDay6/jsexecutor/AWikiWD.java
+++ src/test/java/webDriverDay6/jsexecutor/AWikiWD.java
@@ -72,6 +72,50 @@ public void setUp() throws Exception {
     }
   }
 
+
+  @Test
+  public void testTCWikiKetantest() {
+    	  driver.get(baseUrl);
+//    driver.findElement(By.cssSelector("strong")).click();
+    
+    WebElement englishLink= driver.findElement(By.id("js-link-box-en"));
+    WebElementExtender.highlightElement(englishLink, driver);
+    englishLink.click();
+    try {
+		Thread.sleep(2000);
+	} catch (InterruptedException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}
+    WebElementExtender.highlightElement(driver.findElement(By.id("searchInput")), driver);
+    driver.findElement(By.id("searchInput")).clear();
+    driver.findElement(By.id("searchInput")).sendKeys("QTP");
+    try {
+		Thread.sleep(2000);
+	} catch (InterruptedException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}
+    WebElementExtender.highlightElement(driver.findElement(By.id("searchButton")), driver);
+    driver.findElement(By.id("searchButton")).click();
+    try {
+		Thread.sleep(2000);
+	} catch (InterruptedException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}
+    try {
+      AssertJUnit.assertEquals("Selenium", driver.findElement(By.cssSelector("span")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    try {
+      AssertJUnit.assertEquals("Selenium - Wikipedia, the free encyclopedia", driver.getTitle());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+  }
+
   @AfterMethod
 public void tearDown() throws Exception {
     driver.quit();
